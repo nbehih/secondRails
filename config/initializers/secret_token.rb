2cc1366acd1e5f49764d6fa3d0442656d9b83577
Nawal Behih <nabehih@gmail.com>
2012-02-29 10:32:14 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondRails::Application.config.secret_token = 'b0c39b290897eb0ad433f99bceea8f7008595de23fba6fc6857da24d2812f758d76fcccaedcec61eba98c586bc3c49d1dd2893254e0be1b9b51f459ce1db6aac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SecondRails::Application.config.session_store :cookie_store, key: '_secondRails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SecondRails::Application.config.session_store :active_record_store
